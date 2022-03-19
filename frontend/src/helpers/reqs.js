761623d00ba15f8961502892ad853903f3a47445
Richard E <thedrummer_88@hotmail.com>
2022-03-19 23:00:16 +0800

    Added request handler to my helpers.

---
+++ frontend/src/helpers/reqs.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export function isResponseOK(response) {
+  console.log(response);
+  if (response.status >= 200 && response.status <= 299) {
+    if (response.data) return response.data;
+    return response;
+  } else if (response.status >= 400 && response.status <= 499) {
+    if (response.data) return response.data;
+    return response;
+  } else {
+    throw new Error("Response ERROR!");
+  }
+}
+
+export class RequestHandler {
+  constructor(url, method = "GET", contentType = "application/json") {
+    this.url = url;
+    this.method = method;
+    this.contentType = contentType;
+    this.reqConf = {
+      method: this.method,
+      url: this.url,
+      headers: {
+        "Content-Type": this.contentType,
+      },
+      credentials: "same-origin",
+    };
+  }
+  async sendRequest() {
+    const response = await axios(this.reqConf);
+    let data = isResponseOK(response);
+    return data;
+  }
+}
