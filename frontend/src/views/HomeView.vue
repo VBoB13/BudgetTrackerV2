8e3db0267d099a3165bef3a7b1ea3bd85eb528d2
Richard E <thedrummer_88@hotmail.com>
2022-03-22 14:01:40 +0800

    First tweak to frontend.

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+console.log("Hi mom!");
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Budget Tracker</h1>
   </main>
 </template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
