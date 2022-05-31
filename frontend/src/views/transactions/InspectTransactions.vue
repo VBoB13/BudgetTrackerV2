337b438f1d6b092893ff27d0ffcd611565465098
Richard Ericson <thedrummer_88@hotmail.com>
2022-05-31 11:49:04 +0800

    Added this view to support inspecting and editting individual transactions.

---
+++ frontend/src/views/transactions/InspectTransactions.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <section class="inspect">
+        <h1>Inspect & Edit Transactions</h1>
+        <!-- TransactionList HERE -->
+        <!-- TransactionDetail HERE -->
+    </section>
+</template>
