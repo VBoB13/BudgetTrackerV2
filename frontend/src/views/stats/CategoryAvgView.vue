b5d7113011b7f76ef7d3663a5f18ba6b8dda3138
Richard Ericson <thedrummer_88@hotmail.com>
2022-06-29 12:17:58 +0800

    Laid out the groundwork for making frontend graphs.

--- frontend/src/views/stats/CategoryAvgView.vue
+++ frontend/src/views/stats/CategoryAvgView.vue
@@ -1,12 +1,23 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { RequestHandler } from "../../helpers/reqs";
+import CheckBox from "../../components/forms/inputs/CheckBox.vue";
 
 const img_yes = ref(true);
 const img = ref(false);
 
 function isImage(data) {
   return data.length > 0;
+}
+
+const checkbox_props = {
+  id: "image_check",
+  name: "image_check",
+  text: "Download image from backend? (lower quality but faster)",
+};
+
+function toggle_img() {
+  img_yes.value = document.getElementById("image_check").checked;
 }
 
 function get_category_sum_graph() {
@@ -35,5 +46,6 @@ onMounted(() => {
     <h3>Category Avg.</h3>
     <img v-if="img !== false" :src="img" alt="Category avg. graph" />
     <h5 v-else>No graph yet.</h5>
+    <CheckBox v-bind="checkbox_props" />
   </section>
 </template>
